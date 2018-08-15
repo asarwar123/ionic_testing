7a054af0368c70380d8601c8e06123320cb4abe6
asarwar123 <42272184+asarwar123@users.noreply.github.com>
2018-08-15 13:58:59 +0500

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { RegisterVehiclePage } from '../pages/register-vehicle/register-vehicle';
+import { RegisterMilagePage } from '../pages/register-milage/register-milage';
+import { LogMaintinancePage } from '../pages/log-maintinance/log-maintinance';
+import { SettingsPage } from '../pages/settings/settings';
+import { ReportsPage } from '../pages/reports/reports';
 
 @Component({
   templateUrl: 'app.html'
@@ -22,7 +26,11 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Add Vehicle', component: RegisterVehiclePage },
+      { title: 'Add Milage', component: RegisterMilagePage },
+      { title: 'Log Maintinance', component: LogMaintinancePage },
+      { title: 'Reports', component: ReportsPage },
+      { title: 'settings', component: SettingsPage }
     ];
 
   }
@@ -39,6 +47,7 @@ export class MyApp {
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    //this.nav.setRoot(page.component);
+    this.nav.push(page.component);    
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,31 +4,66 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { LogMaintinancePage } from '../pages/log-maintinance/log-maintinance';
+import { RegisterMilagePage } from '../pages/register-milage/register-milage';
+import { RegisterVehiclePage } from '../pages/register-vehicle/register-vehicle';
+import { ReportsPage } from '../pages/reports/reports';
+import { SettingsPage } from '../pages/settings/settings';
+import { VehicleMakePage } from '../pages/vehicle-make/vehicle-make';
+import { VehiclemodelPage } from '../pages/vehiclemodel/vehiclemodel';
+import { VehicletypePage } from '../pages/vehicletype/vehicletype';
+import { VehiclecolorPage } from '../pages/vehiclecolor/vehiclecolor';
+import { ServicetypeselectorPage } from '../pages/servicetypeselector/servicetypeselector';
+import { ServiceselectorPage } from '../pages/serviceselector/serviceselector';
+import { ServicelogProvider } from '../providers/servicelog/servicelog';
+import {IonicStorageModule} from '@ionic/storage';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    LogMaintinancePage,
+    RegisterMilagePage,
+    RegisterVehiclePage,
+    VehicleMakePage,
+    VehiclemodelPage,
+    ReportsPage,
+    SettingsPage,
+    VehicletypePage,
+    VehiclecolorPage,
+    ServicetypeselectorPage,
+    ServiceselectorPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    LogMaintinancePage,
+    RegisterMilagePage,
+    RegisterVehiclePage,
+    VehicleMakePage,
+    VehiclemodelPage,
+    ReportsPage,
+    SettingsPage,
+    VehicletypePage,
+    VehiclecolorPage,
+    ServicetypeselectorPage,
+    ServiceselectorPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ServicelogProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ServicelogProvider
   ]
 })
 export class AppModule {}
